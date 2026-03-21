202b3418684a10ea9ac5755f4ea3363aec55faaa
JUSEPE11 <52135664+JUSEPE11@users.noreply.github.com>
2026-03-20 21:35:45 -0300

    fix user orders endpoint

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,25 +13,31 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="es" className="h-full">
       <head>
+        {/* Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            __html: `
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id=GTM-WGZSFVJH'+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WGZSFVJH');`,
+})(window,document,'script','dataLayer','GTM-WGZSFVJH');
+`,
           }}
         />
+        {/* End Google Tag Manager */}
       </head>
 
       <body className="min-h-screen h-full bg-[#070709] text-white antialiased overflow-x-hidden selection:bg-yellow-300/30 selection:text-white">
+
+        {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-WGZSFVJH"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
-          />
+          ></iframe>
         </noscript>
 
         <Providers>
