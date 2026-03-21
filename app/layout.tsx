b444dc1a1fde0a9bab07afb019583a511a7035dc
JUSEPE11 <52135664+JUSEPE11@users.noreply.github.com>
2026-03-20 22:03:57 -0300

    fix user orders endpoint

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,19 +17,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <script
           dangerouslySetInnerHTML={{
             __html: `
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id=GTM-WGZSFVJH'+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WGZSFVJH');
-`,
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WGZSFVJH');
+            `,
           }}
         />
         {/* End Google Tag Manager */}
       </head>
 
       <body className="min-h-screen h-full bg-[#070709] text-white antialiased overflow-x-hidden selection:bg-yellow-300/30 selection:text-white">
-
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
